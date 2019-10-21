e6d9bf2a296f0787707e7dda94a69fea9a6b33ce
Phuong <phuongdn996@gmail.com>
2019-10-21 09:55:45 +0700

    bt

---
+++ Module2/LopVaDoiTuong/src/LopHinhChuNhat/LopHinhChuNhat.java
@@ -0,0 +1,40 @@
+package LopHinhChuNhat;
+
+public class LopHinhChuNhat {
+    private double width;
+    private double height;
+
+    public LopHinhChuNhat(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public LopHinhChuNhat() {
+
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public double getArea(){
+        return this.width*this.height;
+    }
+    public double getPerimeter(){
+        return (this.height*this.width)*2;
+    }
+    public String display(){
+        return width + " " + height;
+    }
+}
